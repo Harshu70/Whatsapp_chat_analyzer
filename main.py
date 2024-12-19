0fa2dd290814ddee3badefc55915fb5281a29b43
Harsh Kumar <harshuraj7070@gmail.com>
2024-12-19 15:13:31 +0530

    msg

---
+++ main.py
@@ -0,0 +1,107 @@
+import streamlit as st
+import preprocesser, stats
+import pathlib
+import seaborn
+import matplotlib.pyplot as plt
+
+def load_css(file_path):
+    with open(file_path) as f:
+        st.html(f"<style>{f.read()}</style>")
+
+css_path = pathlib.Path("style.css")
+load_css(css_path)
+
+st.title("Whatsapp chat analyser")
+
+uploaded_file = st.sidebar.file_uploader("choose a file")
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    data = bytes_data.decode("utf-8")
+    df = preprocesser.preprocess(data)
+
+    st.dataframe(df)
+
+    #user dropdown
+    userList = df['user'].unique().tolist()
+    userList.remove('group_notification')
+    userList.sort()
+    userList.insert(0, "Overall")
+
+    selectedUser = st.sidebar.selectbox("Analysis of ", userList)
+
+
+    if st.sidebar.button("Analysis of"):
+        msgNum, wordNum, mediaNum, linkNum = stats.fetchData(selectedUser, df)
+        c1, c2, c3, c4 = st.columns(4)
+
+        with c1:
+            st.header("Total Message")
+            st.title(msgNum)
+        with c2:
+            st.header("Total Words")
+            st.title(wordNum)
+        with c3:
+            st.header("Media shared")
+            st.title(mediaNum)
+        with c4:
+            st.header("Link shared")
+            st.title(linkNum)
+        
+        st.title("Monthly Timeline")
+        tl = stats.monthly(selectedUser, df)
+        fig, ax = plt.subplots()
+        ax.plot(tl['time'], tl['message'], color='red')
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+
+        #actvity
+        st.title("Activity Stats")
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.header("Most Busy Day")
+            bgDay = stats.weekly_stat(selectedUser, df)
+            fig, ax = plt.subplots()
+            ax.bar(bgDay.index, bgDay.values, color='skyblue')
+            plt.xticks(rotation='vertical')
+            st.pyplot(fig)
+        with col2:
+            st.header("Most Busy Month")
+            bgMonth = stats.monthly_stat(selectedUser, df)
+            fig, ax = plt.subplots()
+            ax.bar(bgMonth.index, bgMonth.values, color='violet')
+            plt.xticks(rotation='vertical')
+            st.pyplot(fig)
+
+        if selectedUser != "Overall":
+            st.markdown(f"### Messages from {selectedUser}")
+            user_df = df[df['user'] == selectedUser][['date', 'message']]
+            st.dataframe(user_df)
+        else:
+            st.title("User Contribution")
+            perc = stats.most_active(df)
+            st.dataframe(perc)
+
+        st.title("Wordcloud")
+        df_wc = stats.wrdCloud(selectedUser, df)
+        fig, ax = plt.subplots()
+        ax.imshow(df_wc)
+        st.pyplot(fig)
+
+        cmn_words = stats.most_cmn_word(selectedUser, df)
+        fig, ax = plt.subplots()
+        ax.barh(cmn_words[0], cmn_words[1])
+        st.title("Most Common Words")
+        st.pyplot(fig)
+
+        st.title("Weekly Heatmap")
+        htMap = stats.heatMap(selectedUser, df)
+        fig, ax = plt.subplots()
+        ax = (seaborn.heatmap(htMap))
+        st.pyplot(fig)
+
+
+
+
+        
+
---
+++ stats.py
@@ -0,0 +1,104 @@
+from urlextract import URLExtract
+import pandas as pd
+from collections import Counter
+from wordcloud import WordCloud
+extract = URLExtract()
+
+def fetchData(user, df):
+
+    if user != 'Overall':
+        df = df[df['user']==user]
+    
+    msgNum = df.shape[0]
+    #no of media
+    mediaNum = df[df['message']=='<Media omitted>\n'].shape[0]
+    #no of words in message
+    words = []
+    for msg in df['message']:
+        words.extend(msg.split())
+
+    #no of links
+    link = []
+    for msg in df['message']:
+        link.extend(extract.find_urls(msg))
+    
+    return msgNum, len(words), mediaNum, len(link)
+
+def most_active(df):
+    counts = df['user'].value_counts().reset_index().rename(columns={'count':'number_of_message'})
+    percentages = (df['user'].value_counts()/df.shape[0] * 100).round(2).reset_index().rename(columns={'count':'percentage_contribution'})
+    
+    result_df = pd.merge(counts, percentages, on='user')
+    
+    return result_df
+
+
+def wrdCloud(user, df):
+    if user != 'Overall':
+        df = df[df['user']==user]
+
+    temp_df = df[df['user']!='group_notification']
+    temp_df = temp_df[temp_df['message'] != '<Media omitted>\n']
+    wc = WordCloud(width=500, height=500, min_font_size=10, background_color='white')
+    df_wc = wc.generate(temp_df['message'].str.cat(sep=" "))
+    return df_wc
+
+def most_cmn_word(user, df):
+
+    f = open('hinglish.txt', 'r')
+    stop_words = f.read()
+
+    if user != 'Overall':
+        df = df[df['user']==user]
+
+    temp_df = df[df['user']!='group_notification']
+    temp_df = temp_df[temp_df['message'] != '<Media omitted>\n']
+
+    words = []
+    for msg in temp_df['message']:
+        for word in msg.lower().split():
+            if word not in stop_words:
+                words.append(word)
+
+    new_df = pd.DataFrame(Counter(words).most_common(20))
+    return new_df
+    
+
+def monthly(user,df):
+    if user != 'Overall':
+        df = df[df['user']==user]
+    df['month_num'] = df['date'].dt.month
+    tl = df.groupby(['year', 'month_num', 'month']).count()['message'].reset_index()
+    time = []
+    for i in range(tl.shape[0]):
+        time.append(tl['month'][i]+'-'+str(tl['year'][i]))
+    tl['time'] = time
+    return tl
+
+def weekly_stat(user, df):
+    if user != 'Overall':
+        df = df[df['user']==user]
+    df['day_name'] = df['date'].dt.day_name()
+    return df['day_name'].value_counts()
+
+def monthly_stat(user, df):
+    if user != 'Overall':
+        df = df[df['user']==user]
+    return df['month'].value_counts()
+
+def heatMap(user, df):
+    if user != 'Overall':
+        df = df[df['user']==user]
+    df['day_name'] = df['date'].dt.day_name()
+    period = []
+    for hour in df[['day_name', 'hour']]['hour']:
+        if hour == 23:
+            period.append(str(hour)+"-"+str('00'))
+        elif hour ==0:
+            period.append(str('00') + "-" + str(hour+1))
+        else:
+            period.append(str(hour) + "-" + str(hour+1))
+
+    df['period'] = period
+    act = df.pivot_table(index='day_name', columns='period', values='message', aggfunc='count').fillna(0)
+    return act
